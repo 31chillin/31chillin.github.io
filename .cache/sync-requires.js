01ebf708d1939360680769d91dd50d51a9fc0fee
kmingu07315 <kmingu07315@users.noreply.github.com>
2021-10-13 22:24:34 +0900

    update

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/mgang731/Documents/GitHub/my-gatsby-blog/31chillin.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/mgang731/Documents/GitHub/my-gatsby-blog/31chillin.github.io/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/mgang731/Documents/GitHub/my-gatsby-blog/31chillin.github.io/src/pages/about.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/mgang731/Documents/GitHub/my-gatsby-blog/31chillin.github.io/src/pages/index.js"))),
+  "component---src-templates-blog-template-js": hot(preferDefault(require("/Users/mgang731/Documents/GitHub/my-gatsby-blog/31chillin.github.io/src/templates/blog-template.js"))),
+  "component---src-templates-category-template-js": hot(preferDefault(require("/Users/mgang731/Documents/GitHub/my-gatsby-blog/31chillin.github.io/src/templates/category-template.js")))
+}
+
